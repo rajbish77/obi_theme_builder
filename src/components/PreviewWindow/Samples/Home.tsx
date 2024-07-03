d4a6b0c6c2a065ef274f78768980516e08b1d369
praveens2709 <praveen1892293@gmail.com>
2024-07-03 18:14:01 +0530

    colors

--- src/components/PreviewWindow/Samples/Home.tsx
+++ src/components/PreviewWindow/Samples/Home.tsx
@@ -1,127 +1,160 @@
-import { styled } from "@mui/material/styles";
-import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import ArrivalProductImg from "../../../images/pic2_resized.jpg";
 import DepartureProductImg from "../../../images/pic3_resized.jpg";
 import BundleProductImg from "../../../images/pic1_resized.jpg";
 import giftCard1 from "../../../images/gift-card-1.png";
 import giftCard2 from "../../../images/gift-card-2.png";
 import visamaster from "../../../images/visa-master-card.png";
+import { styled } from "@mui/material/styles";
 import { Link } from "react-router-dom";
-import React from "react";
 
-const StyledDiv = styled("div")(({ theme }) => ({
-  fontFamily: theme.typography.fontFamily,
-  backgroundColor: theme.palette.background.default,
+const LOGOS_URL = "https://nigeria.reliablesoftjm.com/images/logos/JAM.png";
+
+const StyledDiv = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.body.backgroundcolor,
 }));
 
-const StyledSection = styled("section")(({ theme }) => ({
-  backgroundColor: theme.palette.background.default,
-  color: theme.palette.text.primary,
+const SubfooterContainer = styled(Container)(({ theme }) => ({
+  backgroundColor: theme.palette.subfooter.backgroundcolor,
+  color: theme.palette.subfooter.textcolor,
   textAlign: "center",
+  padding: theme.spacing(2),
 }));
 
-const StyledButton = styled(Button)(({ theme }) => ({
-  backgroundColor: `${theme.palette.primary.main} !important`,
-  color: `${theme.palette.primary.contrastText} !important`,
-  border: `1px solid ${theme.palette.primary.main} !important`,
-  boxShadow: `0 3px 1px -2px ${theme.palette.primary.main}, 0 2px 2px 0 ${theme.palette.primary.main}, 0 1px 5px 0 ${theme.palette.primary.main} !important`,
-  // '&:hover': {
-  //   backgroundColor: `${theme.palette.primary.dark} !important`,
-  //   color: `${theme.palette.primary.contrastText} !important`,
-  //   border: `1px solid ${theme.palette.primary.main} !important`,
-  //   boxShadow: `2px 2px 4px 1px ${theme.palette.primary.main}, 2px 2px 4px 1px ${theme.palette.primary.main}, 2px 2px 4px 1px ${theme.palette.primary.main}, 2px 2px 4px 1px ${theme.palette.primary.main} !important`,
-  // },
+const Heading = styled('h3')(({ theme }) => ({
+  color: theme.palette.body.textcolor,
+  fontFamily: theme.typography.fontFamily,
 }));
 
-function Home() {
+const Subheading = styled('h4')(({ theme }) => ({
+  color: theme.palette.body.textcolor,
+  fontFamily: theme.typography.fontFamily,
+}));
+
+const CardBody = styled(Card.Body)(({ theme }) => ({
+  backgroundColor: theme.palette.body.backgroundcolor,
+  color: theme.palette.body.textcolor,
+  fontFamily: theme.typography.fontFamily,
+}));
+
+const CardText = styled(Card.Text)(({ theme }) => ({
+  color: theme.palette.body.textcolor,
+  fontFamily: theme.typography.fontFamily,
+}));
+
+const CardFooter = styled(Card.Footer)(({ theme }) => ({
+  color: theme.palette.body.textcolor,
+  fontFamily: theme.typography.fontFamily,
+  textAlign: 'center',
+}));
+
+const PrimaryButton = styled(Button)(({ theme }) => ({
+  backgroundColor: `${theme.palette.button.primary.background} !important`,
+  color: `${theme.palette.button.primary.color} !important`,
+  border: `1px solid ${theme.palette.button.primary.border} !important`,
+  boxShadow: `0 3px 1px -2px ${theme.palette.button.primary.boxshadow}, 0 2px 2px 0 ${theme.palette.button.primary.boxshadow}, 0 1px 5px 0 ${theme.palette.button.primary.boxshadow} !important`,
+  '&:hover': {
+    backgroundColor: `${theme.palette.button.primary.hoverbackground} !important`,
+    color: `${theme.palette.button.primary.hovercolor} !important`,
+    boxShadow: `2px 2px 4px 1px ${theme.palette.button.primary.boxhovershadow} !important`,
+  },
+}));
+
+const Home = () => {
   return (
     <StyledDiv>
-      <StyledSection>
+      <section>
         <Container className="py-4">
           <h3 className="mb-3 d-flex justify-content-center">
-            <span className="heading">LOUNGE SERVICES</span>
+            <Heading>LOUNGE SERVICES</Heading>
           </h3>
-          <h4 className="my-3 d-flex justify-content-center">
+          <Subheading className="my-3 d-flex justify-content-center">
             Please select your product
-          </h4>
+          </Subheading>
         </Container>
-      </StyledSection>
-
+      </section>
       <section>
         <Container className="my-4">
           <Row className="d-flex justify-content-center">
             <Col className="col-md-4 d-flex">
-              <Card>
-                <StyledButton type="submit" className="my-2 mx-2">
+              <Card className="w-100">
+                <StyledDiv className="p-2">
+                <PrimaryButton type="submit" className="w-100">
                   BOOK ARRIVAL & DEPARTURE
-                </StyledButton>
-
+                </PrimaryButton>
+                </StyledDiv>
                 <Card.Img
                   className="img-responsive bottom-block"
                   variant="middle"
-                  alt={"Image"}
+                  alt="Image"
                   src={BundleProductImg}
                 />
-                <Card.Body>
-                  <Card.Text className="mb-0">
+                <CardBody>
+                  <CardText>
                     Enjoy your first and last impression of the beautiful island with Jamaicas Personalized and Expedited Airport Concierge Lounge Services where you RELAX. CONNECT and DISCOVER our gateway to paradise.
-                  </Card.Text>
-                </Card.Body>
-
-                <Link to="/" className="btn p-0 no-btn-border">
-                  <Card.Footer className="align-items-center text-center p-3">
+                  </CardText>
+                </CardBody>
+                <Link to="/" className="btn p-0 no-btn-border border-0">
+                  <StyledDiv>
+                  <CardFooter>
                     View Price
-                  </Card.Footer>
+                  </CardFooter>
+                  </StyledDiv>
                 </Link>
               </Card>
             </Col>
             <Col className="col-md-4 d-flex">
-              <Card>
-                <StyledButton type="submit" className="my-2 mx-2">
+              <Card className="w-100">
+                <StyledDiv className="p-2">
+                <PrimaryButton type="submit" className="w-100">
                   BOOK ARRIVAL
-                </StyledButton>
-
+                </PrimaryButton>
+                </StyledDiv>
                 <Card.Img
                   className="img-responsive bottom-block"
                   variant="middle"
-                  alt={"Image"}
+                  alt="Image"
                   src={ArrivalProductImg}
                 />
-                <Card.Body>
-                  <Card.Text className="mb-0">
+                <CardBody>
+                  <CardText>
                     Maximize your time in paradise; enjoy personalized escort services with preferential fast track through immigration and customs whilst you access Jamaicas premier arrival lounge where culture meets comfort.
-                  </Card.Text>
-                </Card.Body>
-
-                <Link to="/" className="btn p-0 no-btn-border">
-                  <Card.Footer className="align-items-center text-center p-3">
+                  </CardText>
+                </CardBody>
+                <Link to="/" className="btn p-0 no-btn-border border-0">
+                  <StyledDiv>
+                  <CardFooter>
                     View Price
-                  </Card.Footer>
+                  </CardFooter>
+                  </StyledDiv>
                 </Link>
               </Card>
             </Col>
             <Col className="col-md-4 d-flex">
-              <Card>
-                <StyledButton type="submit" className="my-2 mx-2">
+              <Card className="w-100">
+                <StyledDiv className="p-2">
+                <PrimaryButton type="submit" className="w-100">
                   BOOK DEPARTURE
-                </StyledButton>
-
+                </PrimaryButton>
+                </StyledDiv>
                 <Card.Img
                   className="img-responsive bottom-block"
                   variant="middle"
-                  alt={"Image"}
+                  alt="Image"
                   src={DepartureProductImg}
                 />
-                <Card.Body>
-                  <Card.Text className="mb-0">
+                <CardBody>
+                  <CardText>
                     Continue your vacation and enjoy our "happy everafter" with preferential fast track, impressive services and top class amenities of our culturally inspired lounges.
-                  </Card.Text>
-                </Card.Body>
-
-                <Link to="/" className="btn p-0 no-btn-border">
-                  <Card.Footer className="align-items-center text-center p-3">
+                  </CardText>
+                </CardBody>
+                <Link to="/" className="btn p-0 no-btn-border border-0">
+                  <StyledDiv>
+                  <CardFooter>
                     View Price
-                  </Card.Footer>
+                  </CardFooter>
+                  </StyledDiv>
                 </Link>
               </Card>
             </Col>
@@ -135,74 +168,138 @@ function Home() {
             <div>
               <Row className="text-center">
                 <Col sm={12} md={4} className="offset-md-4">
-                  <h3>LOUNGE SERVICES</h3>
-                  <h4>For Priority Pass/ Diners Club</h4>
-                  <h5>Please select your desired service</h5>
+                  <Heading>LOUNGE SERVICES</Heading>
+                  <Subheading>For Priority Pass/ Diners Club</Subheading>
+                  <Subheading><h5 className="my-3">Please select your desired service</h5></Subheading>
                 </Col>
               </Row>
               <Col className="offset-md-4 col-md-4 d-flex">
-                <Card>
-                  <StyledButton type="submit" className="my-2 mx-2">
-                    BOOK ARRIVAL
-                  </StyledButton>
-
+                <Card className="w-100">
+                  <StyledDiv className="p-2">
+                  <PrimaryButton type="submit" className="w-100">
+                    PRIORITY PASS/DINERS CLUB BOOK DEPARTURE
+                  </PrimaryButton>
+                  </StyledDiv>
                   <Card.Img
                     className="img-responsive bottom-block"
                     variant="middle"
-                    alt={"Image"}
-                    src={giftCard1}
+                    alt="Image"
+                    src={DepartureProductImg}
                   />
-                  <Card.Body>
-                    <Card.Text className="mb-0">
-                      Enjoy personalized escort services with preferential fast track through immigration and customs, access to premier arrival lounge where culture meets comfort.
-                    </Card.Text>
-                  </Card.Body>
-
-                  <Link to="/" className="btn p-0 no-btn-border">
-                    <Card.Footer className="align-items-center text-center p-3">
+                  <CardBody>
+                    <CardText>
+                      Continue your vacation and enjoy our "happy everafter" with preferential fast track, impressive services and top class amenities of our culturally inspired lounges.
+                    </CardText>
+                  </CardBody>
+                  <Link to="/" className="btn p-0 no-btn-border border-0">
+                    <StyledDiv>
+                    <CardFooter>
                       View Price
-                    </Card.Footer>
+                    </CardFooter>
+                    </StyledDiv>
                   </Link>
                 </Card>
               </Col>
-              <Col className="offset-md-4 col-md-4 d-flex mt-3">
-                <Card>
-                  <StyledButton type="submit" className="my-2 mx-2">
-                    BOOK DEPARTURE
-                  </StyledButton>
-
-                  <Card.Img
-                    className="img-responsive bottom-block"
-                    variant="middle"
-                    alt={"Image"}
-                    src={giftCard2}
-                  />
-                  <Card.Body>
-                    <Card.Text className="mb-0">
-                      Continue your vacation with preferential fast track and enjoy impressive services and top class amenities of our culturally inspired lounges.
-                    </Card.Text>
-                  </Card.Body>
-
-                  <Link to="/" className="btn p-0 no-btn-border">
-                    <Card.Footer className="align-items-center text-center p-3">
-                      View Price
-                    </Card.Footer>
-                  </Link>
-                </Card>
-              </Col>
-              <Row className="text-center">
-                <Col sm={12} md={4} className="offset-md-4">
-                  <img
-                    src={visamaster}
-                    alt="Visa Mastercard"
-                    className="img-responsive img-fluid mt-5"
-                  />
-                </Col>
-              </Row>
             </div>
           </Row>
         </Container>
       </section>
+
+      <section>
+        <Container className="my-4">
+          <Row>
+            <div>
+              <Row className="text-center">
+                <Col sm={12} md={4} className="offset-md-4">
+                  <Subheading>For Digicel Executive Card</Subheading>
+                  <Subheading><h5 className="my-3">Please select your desired service</h5></Subheading>
+                </Col>
+              </Row>
+              <Col className="offset-md-4 col-md-4 d-flex">
+                <Card className="w-100">
+                  <StyledDiv>
+                  <PrimaryButton type="submit" className="my-2 mx-2">
+                    DIGICEL EXECUTIVE CARD BOOK DEPARTURE
+                  </PrimaryButton>
+                  </StyledDiv>
+                  <Card.Img
+                    className="img-responsive bottom-block"
+                    variant="middle"
+                    alt="Image"
+                    src={DepartureProductImg}
+                  />
+                  <CardBody>
+                    <CardText>
+                      Continue your vacation and enjoy our "happy everafter" with preferential fast track, impressive services and top class amenities of our culturally inspired lounges.
+                    </CardText>
+                  </CardBody>
+                  <Link to="/" className="btn p-0 no-btn-border border-0">
+                    <StyledDiv>
+                    <CardFooter>
+                      View Price
+                    </CardFooter>
+                    </StyledDiv>
+                  </Link>
+                </Card>
+              </Col>
+            </div>
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container className="my-4 d-flex justify-content-center">
+          <Card style={{ width: "30rem", height: "auto" }}>
+            <StyledDiv>
+            <Row>
+              <Col sm={12} md={7}>
+                <CardBody>
+                  <Heading>E-GIFT CARD</Heading>
+                  <CardText>
+                    VIP Attractions e-Gift Card is a perfect gift for any occasion! It acts like a debit card – you choose the service you would like to give and the recipient can redeem it when they are ready to travel.
+                  </CardText>
+                  <Link to="/" className="text-decoration-none">
+                    <PrimaryButton type="submit" className="w-100">
+                      Get Started
+                    </PrimaryButton>
+                  </Link>
+                </CardBody>
+              </Col>
+              <Col sm={12} md={5} className="justify-content-center">
+                <img
+                  src={giftCard1}
+                  height="200"
+                  width="124"
+                  className="p-1 mx-3"
+                  alt="Gift Card"
+                />
+                <img
+                  src={giftCard2}
+                  height="100"
+                  width="147"
+                  className="d-inline-block p-1 mx-3"
+                  alt="Gift Card"
+                />
+              </Col>
+            </Row>
+            </StyledDiv>
+          </Card>
+        </Container>
+      </section>
+
+      <SubfooterContainer fluid="xxl">
+        <Row>
+          <Col md="12" className="my-2 p-2">
+            <p>
+              For groups of 25 persons or more, please contact our groups department at +1-876-619-1565 or groups@vipattractions.com. <br />
+              You can also reach us via Skype at 954-837-6290
+            </p>
+            <div className="d-flex justify-content-center">
+              <img width="300" alt="visa mastercard" src={visamaster} />
+            </div>
+          </Col>
+        </Row>
+      </SubfooterContainer>
     </StyledDiv>
   );
 }
--- src/components/PreviewWindow/Samples/NavigationBar.tsx
+++ src/components/PreviewWindow/Samples/NavigationBar.tsx
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { Col, Row, Container } from "react-bootstrap";
-import {
-  menuDefault,
-  vipMenus,
-  menuVIPMembershipCard,
-  menuFeedback,
-  menuGetUpdateFlightDetailsLink,
-} from "./menuItems";
-import { createTheme, styled } from "@mui/material/styles"
+import { Nav, Navbar, NavDropdown, Col, Row, Container } from "react-bootstrap";
+import { menuDefault, vipMenus, menuVIPMembershipCard, menuFeedback, menuGetUpdateFlightDetailsLink } from './menuItems';
+import { styled } from "@mui/material/styles";
 
 interface MenuItem {
   key: string;
@@ -26,6 +17,130 @@ interface SubmenuItem {
   url?: string;
   target?: string;
 }
+
+const LOGOS_URL = "https://nigeria.reliablesoftjm.com/images/logos/JAM.png";
+
+const StyledNavbar = styled(Navbar)(({ theme }) => ({
+  backgroundColor: theme.palette.navbar.backgroundcolor,
+}));
+
+const StyledNav = styled(Nav)(({ theme }) => ({
+  color: theme.palette.navbar.textcolor,
+  fontFamily: theme.typography.fontFamily,
+}));
+
+const StyledNavDropdown = styled(NavDropdown)(({ theme }) => ({
+  color: theme.palette.navbar.textcolor,
+  fontFamily: theme.typography.fontFamily,
+  backgroundColor: theme.palette.navbar.backgroundcolor,
+}));
+
+const StyledNavDropdownItem = styled(NavDropdown.Item)(({ theme }) => ({
+  color: theme.palette.navbar.textcolor,
+  fontFamily: theme.typography.fontFamily,
+  backgroundColor: theme.palette.navbar.backgroundcolor,
+}));
+
+const StyledNavLink = styled(Nav.Link)(({ theme }) => ({
+  color: theme.palette.navbar.textcolor,
+  fontFamily: theme.typography.fontFamily,
+  backgroundColor: theme.palette.navbar.backgroundcolor,
+}));
+
+const TitleSpan = styled('span')(({ theme }) => ({
+  color: theme.palette.navbar.textcolor,
+}));
+
+const Navigationbar = () => {
+  const combinedMenu = [
+    ...menuDefault,
+    ...vipMenus,
+    ...menuVIPMembershipCard,
+    ...menuFeedback,
+    ...menuGetUpdateFlightDetailsLink
+  ];
+  const [menuItems] = useState(combinedMenu);
+
+  return (
+    <>
+      <StyledNavbar expand="lg" sticky="top">
+        <Container fluid="xxl">
+          <Row className="d-flex justify-content-start align-items-center">
+            <Col md={"auto"} className="d-flex pe-0">
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Brand href={"/"} className="ps-1 me-0 ps-md-0">
+                <img
+                  src={LOGOS_URL}
+                  width={200}
+                  className="d-inline-block align-left"
+                  alt={"Jamaica"}
+                />
+              </Navbar.Brand>
+            </Col>
+            <Col md={"auto"} className="d-flex justify-content-between">
+              <Navbar.Collapse id="basic-navbar-nav">
+                <StyledNav>{renderMenuItems(menuItems)}</StyledNav>
+              </Navbar.Collapse>
+            </Col>
+          </Row>
+        </Container>
+      </StyledNavbar>
+    </>
+  );
+};
+
+const renderMenuItems = (menuItems: MenuItem[]) => {
+  return menuItems.map((item, i) => {
+    if (item.submenu) {
+      return (
+        <StyledNavDropdown
+          key={i}
+          id={item.key}
+          renderMenuOnMount={true}
+          title={<TitleSpan>{item.title}</TitleSpan>}
+        >
+          {renderSubMenu(item.submenu)}
+        </StyledNavDropdown>
+      );
+    } else {
+      return (
+        <StyledNavLink key={i} href={item.url}>
+          {item.title}
+        </StyledNavLink>
+      );
+    }
+  });
+};
+
+const renderSubMenu = (submenu: SubmenuItem[] | undefined) => {
+  if (!submenu) return null;
+  return submenu.map((item, i) => {
+    if (item.submenu) {
+      return (
+        <StyledNavDropdown
+          key={i}
+          title={<TitleSpan>{item.title}</TitleSpan>}
+          id={item.key}
+          renderMenuOnMount={true}
+        >
+          {renderSubMenu(item.submenu)}
+        </StyledNavDropdown>
+      );
+    } else {
+      return (
+        <StyledNavDropdownItem
+          key={i}
+          href={item.url}
+          target={item.target}
+        >
+          {item.title}
+        </StyledNavDropdownItem>
+      );
+    }
+  });
+};
+
+export default Navigationbar;
 
 declare module '@mui/material/styles' {
   interface Palette {
@@ -105,113 +220,3 @@ declare module '@mui/material/styles' {
     },
   }
 }
-
-
-const StyledNavbar = styled(Navbar)(({ theme }) => ({
-  backgroundColor: theme.palette.navbar?.backgroundcolor,
-}));
-
-const StyledNav = styled(Nav)(({ theme }) => ({
-  color: theme.palette.navbar?.textcolor,
-}));
-
-const Navigationbar = () => {
-  const combinedMenu = [
-    ...menuDefault,
-    ...vipMenus,
-    ...menuVIPMembershipCard,
-    ...menuFeedback,
-    ...menuGetUpdateFlightDetailsLink,
-  ];
-  const [menuItems, setMenuItems] = useState<MenuItem[]>(combinedMenu);
-  const LOGOS_URL = "https://nigeria.reliablesoftjm.com/images/logos/JAM.png";
-
-  return (
-    <>
-      <StyledNavbar expand="lg" sticky="top">
-        <Container fluid="xxl">
-          <Row className="d-flex justify-content-start align-items-center">
-            <Col md={"auto"} className="d-flex pe-0">
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Brand href={"/"} className="ps-1 me-0 ps-md-0">
-                <img
-                  src={LOGOS_URL}
-                  width={200}
-                  className="d-inline-block align-left"
-                  alt={"Jamaica"}
-                />
-              </Navbar.Brand>
-            </Col>
-            <Col md={"auto"} className="d-flex justify-content-between">
-              <Navbar.Collapse id="basic-navbar-nav">
-                <StyledNav className="nav-link">
-                  {renderMenuItems(menuItems)}
-                </StyledNav>
-              </Navbar.Collapse>
-            </Col>
-          </Row>
-        </Container>
-      </StyledNavbar>
-    </>
-  );
-};
-
-const renderMenuItems = (menuItems: MenuItem[]) => {
-  return menuItems.map((item, i) => {
-    if (item.submenu) {
-      return (
-        <NavDropdown
-          key={i}
-          title={<span>{item.title}</span>}
-          id={item.key}
-          renderMenuOnMount={true}
-          className="nav-link"
-        >
-          {renderSubMenu(item.submenu)}
-        </NavDropdown>
-      );
-    } else {
-      return (
-        <Nav.Link
-          key={i}
-          href={item.url}
-          className="nav-link"
-        >
-          {item.title}
-        </Nav.Link>
-      );
-    }
-  });
-};
-
-const renderSubMenu = (submenu: SubmenuItem[] | undefined) => {
-  if (!submenu) return null;
-  return submenu.map((item, i) => {
-    if (item.submenu) {
-      return (
-        <NavDropdown
-          key={i}
-          title={<span>{item.title}</span>}
-          id={item.key}
-          renderMenuOnMount={true}
-          className="nav-link"
-        >
-          {renderSubMenu(item.submenu)}
-        </NavDropdown>
-      );
-    } else {
-      return (
-        <NavDropdown.Item
-          key={i}
-          href={item.url}
-          className="nav-link"
-          target={item.target}
-        >
-          {item.title}
-        </NavDropdown.Item>
-      );
-    }
-  });
-};
-
-export default Navigationbar;
